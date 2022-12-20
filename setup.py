0d0bed9bc4ec3f38261f8fa87f2a2968b710bdf3
hieupth <64821726+hieupth@users.noreply.github.com>
2022-12-20 23:27:35 +0700

    Create package

---
+++ setup.py
@@ -0,0 +1,57 @@
+# -----------------------------------------------------------------------------
+# MIT License
+#
+# Copyright (c) 2022 Gradients Technologies. All rights reserved.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -----------------------------------------------------------------------------
+
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.md', encoding='utf-8') as file:
+    description = file.read()
+
+setup(
+    name='simpleqa',
+    version='0.1.1',
+    packages=find_packages(),
+    license='MIT',
+    zip_safe=True,
+    description='Python simple Question-Answer package based on text embeddings similarity method.',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author='Hieu Pham',
+    author_email='64821726+hieupth@users.noreply.github.com',
+    url='https://gitlab.com/hieupth/simpleqa',
+    keywords=[],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
