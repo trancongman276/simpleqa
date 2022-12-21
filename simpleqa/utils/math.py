58b5b177498f579b88f97d5ca88382b116ed87d2
hieupth <64821726+hieupth@users.noreply.github.com>
2022-12-21 09:41:59 +0700

    feat: tensor normalize function

---
+++ simpleqa/utils/math.py
@@ -0,0 +1,35 @@
+# -----------------------------------------------------------------------------
+# MIT License
+#
+# Copyright (c) 2022 Gradients Technologies. All rights reserved.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# -----------------------------------------------------------------------------
+
+import torch
+
+def norm(x: torch.Tensor, dim: int = 1, **kwargs) -> torch.Tensor:
+    """
+    Normalize a tensor.
+    :param x:       input torch tensor.
+    :param dim:     dimension to be normalized.
+    :param kwargs:  additional keyword arguments.
+    :return:        normalized torch tensor.
+    """
+    return x / x.norm(dim=dim)[:, None]
